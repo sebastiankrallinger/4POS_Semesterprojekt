7b3712733fe6ae296a34a8819dde0743dfc95b4b
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2024-03-07 11:21:59 +0100

    create server/files mongodb

---
+++ WebApp/src/main/java/com/example/webapp/dtos/UserDto.java
@@ -0,0 +1,11 @@
+package com.example.webapp.dtos;
+
+import org.bson.types.ObjectId;
+
+public record UserDto (String id, String username, String password){
+    /*public UserDto() {
+        this();
+    }*/
+
+
+}
