88a75c850aff41f1c7300df6c6e1493f6be02432
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2024-03-11 12:10:38 +0100

    add js-file

---
+++ WebApp/src/main/resources/static/login.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Füge den Event Listener erst hinzu, wenn das DOM vollständig geladen ist
+    document.getElementById('user').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var username = "example_user";
+        var password = "example_password";
+
+        var data = {
+            username: username,
+            password: password
+        };
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/app/user', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 201) {
+                console.log('User created successfully');
+                window.location.href = '/app/mainPage';
+            }
+        };
+        xhr.send(JSON.stringify(data));
+    });
+});
