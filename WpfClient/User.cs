9de4125e58cb5f9cc6301c93325cc6705372184b
sebastiankrallinger <sebastian.krallinger@htl-saalfelden.at>
2024-06-02 18:12:11 +0200

    remove comment

--- WpfClient/User.cs
+++ WpfClient/User.cs
@@ -4,7 +4,6 @@ namespace WpfClient
 {
     class User
     {
-        //Properties der Eigenschaften
         public string id {  get; set; }
         public string username { get; set; }
         public string password { get; set; }
